275a80edb39e0928930e96e53709cd92f9262226
Emanuel Marroquin <carlosema322@gmail.com>
2025-03-25 14:32:27 -0600

    Added dynamic data passing from controller to view

--- mvc-web-apps/Controllers/HomeController.cs
+++ mvc-web-apps/Controllers/HomeController.cs
@@ -2,30 +2,33 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using mvc_web_apps.Models;
 
-namespace mvc_web_apps.Controllers;
-
-public class HomeController : Controller
+namespace mvc_web_apps.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
+        private readonly ILogger<HomeController> _logger;
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+        public IActionResult Index()
+        {
+            ViewData["Message"] = "Welcome to Ema322's page!";
+            ViewData["UserName"] = "Ema";
+            return View();
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
